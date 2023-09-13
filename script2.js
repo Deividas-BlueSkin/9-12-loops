256822d6b052ff04f72049b98d40f0b81f915f07
Deividas-BlueSkin <somecrazyp@gmail.com>
2023-09-13 15:42:36 +0300

    commit

---
+++ script2.js
@@ -0,0 +1,75 @@
+let arrayA = [1, 2, 3, 3, 4, 5, 6]
+
+// console.log(arrayA)
+// console.log(arrayA.length)
+//console.log(arrayA[0])
+
+
+// let arrayB = ['bo', 15, 1.4,'moon',13574.8196541232179,[55,66,77,88,99]]
+
+// console.log(arrayB[5][4])
+
+// arrayB[0] = 'Sun Wu'
+// console.log(arrayB[0])
+
+// arrayB[6] = 'Land of Sorrow'
+
+//bad way
+// arrayB[arrayB.length] = 'Land of Shadow'
+
+// console.log(arrayB.pop())
+// arrayB.push('vomit')
+// arrayB.PowB ='Ooze vomit'
+
+// arrayB.shift()
+// console.log(arrayB.shift())
+// console.log(arrayB.unshift())
+// console.log(arrayB)
+
+// console.log(arrayB)
+// arrayB.slice(2)
+
+
+// console.log(arrayB.slice(2).shift())
+// console.log(arrayB.splice(2,1,'Ghohoohhhhhho'))
+// console.log(arrayB)
+
+// [<=-=-=-=-=-=-=-=-=-=-=-=>][<=-=-=-=-=-=-=-=-=-=-=-=>][<=-=-=-=-
+// task
+// [<=-=-=-=-=-=-=-=-=-=-=-=>][<=-=-=-=-=-=-=-=-=-=-=-=>][<=-=-=-=-
+
+console.log(`[<=-=-=-=-=-=-=-=-=-=-=-=>][<=-=-=-=-=-=-=-=-=-=-=->]`)
+
+let arr = [4, 5, -5556, 155, 640, '15x', 6789,
+    -5564, 478, 654, 'apple', 789, -51, 55, -222,
+    0, -357, -56, 'change', 444, 7778, 4154, 4751]
+
+arr.shift()//ok
+arr.pop()//not hard
+console.log(`${arr.length} mid -> ${(arr.length) / 2}`)//making strategy
+arr.splice(Math.trunc(arr.length) / 2, 1)//seems hard
+arr.splice(-2, 1)//it's confusing
+arr.splice(1, 1)//going insane
+arr.splice(6, 2)// mentally ill
+arr.splice(-6, 3)//I am deranged
+arr.splice(2, 1, 888)//I'm now demented 
+arr.splice(9, 1, 33, 789, 6543)// not of sound mind
+arr.splice(-1, 1, 321, 654, 987)//crazy & dull
+arr.splice(1, 0, 11)// shifting lunatic
+arr.splice(13, 0, 1)//became hysterical
+arr.splice(-1, 0, -1)//kind'a delirious
+arr.unshift(1, 2, 3)//mental circumstance - I'm foaming at the mouth
+arr.push(-333, -321, -312)// gone bananas
+arr.splice(Math.trunc(arr.length) / 2, 0, 0)// loopy loony dippy screwy
+arr.shift()// There lived a certain man in Russia long ago
+arr.pop()//He was big and strong, in his eyes a flaming glow
+arr.unshift('start')//Most people looked at him with terror and with fear
+arr.push('end')// But to Moscow chicks he was such a lovely dear
+
+console.log(arr)// Ra ra Rasputin
+
+// [<=-=-=-=-=-=-=-=-=-=-=-=>][<=-=-=-=-=-=-=-=-=-=-=-=>][<=-=-=-=-
+// next task
+// [<=-=-=-=-=-=-=-=-=-=-=-=>][<=-=-=-=-=-=-=-=-=-=-=-=>][<=-=-=-=-
+
+//no
